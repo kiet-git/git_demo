35ddf1f02c96dbab7d49b3829c397ab1e454b863
Troy <hoangtrieu11nc@gmail.com>
2024-09-14 14:53:15 +0700

    modified: main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(1000):
+import random
+while True:
     print("Troy is playing chess!")
     print("Jack is also playing chess!")
+    print(f"{"Troy" if random.randint("1", "2") == "1" else "Jack"} had won the game!")
