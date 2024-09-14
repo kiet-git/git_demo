3df8e725c3f40c9d3e178327c53aa20d501d78cf
Troy <hoangtrieu11nc@gmail.com>
2024-09-14 15:00:48 +0700

    modified:   main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import random
 while True:
     print("Troy is playing chess!")
     print("Jack is also playing chess!")
-    print(f"{"Troy" if random.randint("1", "2") == "1" else "Jack"} had won the game!")
+    print(f'{"Troy" if random.randint(1, 2) == 1 else "Jack"} had won the game!')
