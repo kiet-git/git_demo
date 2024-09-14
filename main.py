f2bd81b3f96508a32d25dfd73a472cc5c722997b
Harry-NTMK <lam14122009@gmail.com>
2024-09-14 14:57:15 +0700

    harrie no fat

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-for i in range(1000):
-    print("Troy is playing chess!")
+#for i in range(1000):
+    #print("Troy is playing chess!")
 
-    print("Jack is also playing chess!")
+    #print("Jack is also playing chess!")
+
+for i in range (50):
+    print ("if you read this line, you will forgot u were gei")
+    
+    if i > 20:
+        print("hehe")
